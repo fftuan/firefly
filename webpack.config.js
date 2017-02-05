842fbf24019f28b7551f2a129df439f687342e2f
laochengplus <190537739@qq.com>
2017-02-05 16:08:38 +0800

    Add files via upload
    initial commit, create a 'hello world' page as index page.
    you should use 'npm start' to start this service and visit the index page through 'http://127.0.0.1:8080/index.html'(might be different in your environment) by then.

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+
+module.exports = {
+    entry: {
+        app: path.resolve(APP_PATH, 'index.js'),
+        utils: ['jquery', 'lodash']
+    },
+    output: {
+        path: BUILD_PATH,
+        filename: '[name].js'
+    },
+    devtool: 'eval-source-map',
+    devServer: {
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        progress: true
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loaders: ['style', 'css'],
+                include: APP_PATH
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                include: APP_PATH,
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'firefly',
+            template: path.resolve(APP_PATH, 'index.html'),
+            chunks: ['app', 'utils'],
+            inject: 'body'
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            _: 'lodash',
+            lodash: 'lodash'
+        })
+    ],
+    resolve: {
+        alias: {
+            
+        }
+    }
+};
