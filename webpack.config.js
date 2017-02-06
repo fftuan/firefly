3bb73ae033428b7afdcb4fe94658b54d0bfc53b9
frosv <616342684@qq.com>
2017-02-06 14:15:04 +0800

    更新HTML页面代码，添加package的插件，修改webpack可以查看断点

--- webpack.config.js
+++ webpack.config.js
@@ -7,55 +7,55 @@ var APP_PATH = path.resolve(ROOT_PATH, 'app');
 var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
 
 module.exports = {
-    entry: {
-        app: path.resolve(APP_PATH, 'index.js'),
-        utils: ['jquery', 'lodash']
-    },
-    output: {
-        path: BUILD_PATH,
-        filename: '[name].js'
-    },
-    devtool: 'eval-source-map',
-    devServer: {
-        historyApiFallback: true,
-        hot: true,
-        inline: true,
-        progress: true
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.css$/,
-                loader: 'style!css',
-                include: APP_PATH
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel',
-                include: APP_PATH,
-                query: {
-                    presets: ['es2015']
-                }
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'firefly',
-            template: path.resolve(APP_PATH, 'index.html'),
-            chunks: ['app', 'utils'],
-            inject: 'body'
-        }),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            _: 'lodash',
-            lodash: 'lodash'
-        })
-    ],
-    resolve: {
-        alias: {
-
+  devtool: 'source-map',
+  entry: {
+    app: path.resolve(APP_PATH, 'index.js'),
+    utils: ['jquery', 'lodash']
+  },
+  output: {
+    path: BUILD_PATH,
+    filename: '[name].js'
+  },
+  // devtool: 'eval-source-map',
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true
+  },
+  module: {
+    loaders: [{
+        test: /\.css$/,
+        loader: 'style!css',
+        include: APP_PATH
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        include: APP_PATH,
+        query: {
+          presets: ['es2015']
         }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'firefly',
+      template: path.resolve(APP_PATH, 'index.html'),
+      chunks: ['app', 'utils'],
+      inject: 'body'
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      _: 'lodash',
+      lodash: 'lodash'
+    })
+  ],
+  resolve: {
+    alias: {
+
     }
+  }
 };
