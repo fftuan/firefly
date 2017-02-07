d0581392601f95bbb2a23cbc943b2770aae561f1
frosv <616342684@qq.com>
2017-02-07 17:04:40 +0800

    播放器插件封装架构(非严格模式)

--- app/index.js
+++ app/index.js
@@ -1,11 +1,38 @@
+// 引入基本样式文件
 require('./index.css');
 require('../node_modules/bootstrap/dist/css/bootstrap.min.css');
-// require('bootstrap');
-
-
-
-// var Media = document.getElementById("danmuPlayer");
-console.log(111);
-
 
 var media = $('#danmuPlayer');
+
+// 封装播放器插件
+
+(function($) {
+  var pluginName = 'danmuPlayer';
+  // 默认参数配置
+  var defaults = {};
+
+  // 缓存参数
+  function Plugin(element, options) {
+    this.options = $.extend({}, defaults, options);
+    this.init();
+  };
+
+  // 原型功能
+  Plugin.prototype.init = function() {};
+
+  // 暴露接口
+  $.fn[pluginName] = function(options) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    return this.each(function() {
+      var $this = $(this),
+        data = $this.data('plugin_' + pluginName);
+      if (!data) {
+        $this.data('plugin_' + pluginName, (data = new Plugin(this, options)));
+      };
+      if (typeof options === 'string') {
+        data[options].apply(data, args);
+      };
+    });
+  };
+
+})(jQuery);
