5056aaac55e07ca5c6e14bf6d05199ce0df640e0
Frosv <616342684@qq.com>
2017-02-12 21:13:31 +0800

    整理代码

--- app/index/js/send.js
+++ app/index/js/send.js
@@ -1,6 +1,6 @@
 var danmuRoll = require('./danmu.js');
 //发送弹幕按钮
-$('#sendDanmu').on('click',function(){
+$('#sendDanmu').on('click', function() {
   var text = $.trim($('#danmuInput').val());
   danmuRoll(text);
   $('#danmuInput').val('');
