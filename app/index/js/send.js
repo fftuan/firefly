bddc2cae211e916f014134f43b5cc2264a503a0b
frosv <616342684@qq.com>
2017-02-20 10:48:47 +0800

    增加输入空的判断

--- app/index/js/send.js
+++ app/index/js/send.js
@@ -1,7 +1,12 @@
 var danmuRoll = require('./danmu.js');
 //发送弹幕按钮
 $('#sendDanmu').on('click', function() {
-  var text = $.trim($('#danmuInput').val());
-  danmuRoll(text);
-  $('#danmuInput').val('');
+  var _thisTxt = $.trim($('#danmuInput').val());
+  if (_thisTxt) {
+    danmuRoll(_thisTxt);
+    $('#danmuInput').val('');
+  } else {
+    console.log('输入内容无法为空！');
+    return;
+  }
 });
