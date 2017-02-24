5f2ab7f9d5aa2bfb97ba92140de1dac7b412c188
frosv <616342684@qq.com>
2017-02-24 17:26:31 +0800

    加入/你妈/替换，布局小修改

--- app/index/js/send.js
+++ app/index/js/send.js
@@ -2,6 +2,9 @@ var danmuRoll = require('./danmu.js');
 //发送弹幕按钮
 $('#sendDanmu').on('click', function() {
   var _thisTxt = $.trim($('#danmuInput').val());
+  if(/你妈/.test(_thisTxt)){
+    $('#inputError').text(_thisTxt.replace(/你妈/,'我妈'));
+  }
   if (_thisTxt) {
     danmuRoll(_thisTxt);
     $('#danmuInput').val('');
